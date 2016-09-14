4e500374cc0d58bd85bf8de29b1f5c0f2bcf3111
homelleon <ihomelleoni@yandex.ru>
2016-09-14 16:54:43 +0300

    OBJ Loader

--- src/renderEngine/Renderer.java
+++ src/renderEngine/Renderer.java
@@ -29,9 +29,9 @@ public class Renderer {
 	}
 	
 	public void prepare(){
-		
-		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
-		GL11.glClearColor(1, 0, 0, 1);
+		GL11.glEnable(GL11.GL_DEPTH_TEST);
+		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT|GL11.GL_DEPTH_BUFFER_BIT);
+		GL11.glClearColor(0, 0, 0, 0);
 		
 		
 	}
