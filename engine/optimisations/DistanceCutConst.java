3b5f8ec7b497b8bcde0c7b63c90320f278874ec9
homelleon <ihomelleoni@yandex.ru>
2016-11-10 18:08:09 +0300

    optimisation

---
+++ engine/optimisations/DistanceCutConst.java
@@ -0,0 +1,53 @@
+package optimisations;
+
+import java.util.List;
+
+import org.lwjgl.util.vector.Vector3f;
+
+import entities.Camera;
+import entities.Entity;
+import scene.Settings;
+import terrains.Terrain;
+import toolbox.Maths;
+
+//Cut during to constants
+public class DistanceCutConst {
+
+	public void cutRender(Camera camera, List<Entity> entities, List<Terrain> terrains) {
+		cutEntityRender(camera, entities);	
+		cutTerrainRender(camera, terrains);
+	}
+	
+	private void cutEntityRender(Camera camera, List<Entity> entities) {
+		for(Entity entity : entities) {
+			if(entity.isVisible()) {
+				float distance = Maths.distanceFromCamera(entity, camera);
+				if(distance <= Settings.RENDERING_VIEW_DISTANCE) {
+					entity.setRendered(true);
+				} else {
+					entity.setRendered(false);
+				}
+			} else {
+				entity.setRendered(false);
+			}
+			
+		}
+	}
+	
+	private void cutTerrainRender(Camera camera, List<Terrain> terrains) {
+		for(Terrain terrain : terrains) {
+			if(terrain.isVisible()) {
+				float distance = Maths.distanceFromCamera(terrain, camera);
+				if(distance <= 1000) {
+					terrain.setRendered(true);
+				} else {
+					terrain.setRendered(false);
+				}
+			} else {
+				terrain.setRendered(false);
+			}
+			
+		}
+	}
+	
+}
---
+++ engine/optimisations/Optimisation.java
@@ -0,0 +1,13 @@
+package optimisations;
+
+import java.util.List;
+
+import entities.Camera;
+import entities.Entity;
+import terrains.Terrain;
+
+public interface Optimisation {
+	
+	public void optimize(Camera camera, List<Entity> entities, List<Terrain> terrains);
+	
+}
