acc7a112902a12c2e34519c9a576f1b6f71edba1
homelleon <ihomelleoni@yandex.ru>
2017-07-26 18:59:28 +0300

    [core] add GUIobject map into GUIMenu class

--- engine/object/gui/control/menu/GUIMenu.java
+++ engine/object/gui/control/menu/GUIMenu.java
@@ -1,5 +1,12 @@
 package object.gui.control.menu;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import object.gui.control.object.GUIObject;
+
 public class GUIMenu {
+	
+	Map<String, GUIObject> objects = new HashMap<String, GUIObject>();
 
 }
