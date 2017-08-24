062ef763594390f3a1d23a3061d2c6921946418b
homelleon <ihomelleoni@yandex.ru>
2017-08-24 20:56:21 +0300

    [refactor] try to use Sream API.

--- src/main/java/engine/manager/entity/EntityManager.java
+++ src/main/java/engine/manager/entity/EntityManager.java
@@ -2,11 +2,9 @@ package manager.entity;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
