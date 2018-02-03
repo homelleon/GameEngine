d13ea6ded2a08044625ae9c06676cb09a7090f4f
homelleon <ihomelleoni@yandex.ru>
2018-02-03 13:00:22 +0300

    [Doc/Refactor] clean up Light class and ILight interface.

--- src/main/java/engine/object/light/ILight.java
+++ src/main/java/engine/object/light/ILight.java
@@ -4,18 +4,49 @@ import object.Nameable;
 import tool.math.vector.Vector3f;
 
 /**
+ * Light source interface.
  * 
  * @author homelleon
  * @version 1.0
  * @see Light
  */
 public interface ILight extends Nameable {
-	
-	// вернуть затухание
+	/**
+	 * Gets 3 type of attenuation value.<br>
+	 * Use x for constant attenuation.<br> 
+	 * Use y for linear attenuation.<br>
+	 * Use z for square attenuation.<br>
+	 *
+	 * @return {@link Vector3f} value
+	 */
 	public Vector3f getAttenuation();
+	
+	/**
+	 * Gets position of current light source.
+	 * 
+	 * @return {@link Vector3f} value
+	 */
 	public Vector3f getPosition();
+	
+	/**
+	 * Sets current light source position.
+	 * 
+	 * @param position {@link Vector3f} value
+	 */
 	public void setPosition(Vector3f position);
+	
+	/**
+	 * Gets current light color value.
+	 * 
+	 * @return {@link Vector3f} value
+	 */
 	public Vector3f getColor();
+	
+	/**
+	 * Sets color for current light source.
+	 * 
+	 * @param color {@link Vector3f} value
+	 */
 	public void setColor(Vector3f color);
 
 }
--- src/main/java/engine/object/light/Light.java
+++ src/main/java/engine/object/light/Light.java
@@ -3,51 +3,73 @@ package object.light;
 import object.GameObject;
 import tool.math.vector.Vector3f;
 
-/*
- * Light - источник света
- * 03.02.17
- * ------------
+/**
+ * Light source object.
+ * 
+ * @author homelleon
+ *
  */
-
 public class Light extends GameObject implements ILight {
 
-	private Vector3f position; // позиция
-	private Vector3f color; // цвеь
-	private Vector3f attenuation = new Vector3f(1, 0, 0); // затухание
+	private Vector3f position;
+	private Vector3f color;
+	private Vector3f attenuation = new Vector3f(0.5f, 0, 0);
 
-	private String name;
-
-	public Light(String name, Vector3f position, Vector3f colour) {
+	/**
+	 * Light constructor without attenuation parameter.<br>
+	 * Attenuation will be setted with 0.5f constant.
+	 *  
+	 * @param name {@link String}
+	 * @param position {@link Vector3f}
+	 * @param color {@link Vector3f}
+	 */
+	public Light(String name, Vector3f position, Vector3f color) {
 		super(name);
 		this.position = position;
-		this.color = colour;
+		this.color = color;
 	}
-
-	public Light(String name, Vector3f position, Vector3f colour, Vector3f attenuation) {
+	
+	/**
+	 * Light constructor with attenuation parameter.<br>
+	 * Attenuation is defined by Vector3f value, where:<br>
+	 * x is for constant;<br>
+	 * y is for linear relation;<br>
+	 * z is for square relation;<br>
+	 *   
+	 * @param name {@link String}
+	 * @param position {@link Vector3f}
+	 * @param color {@link Vector3f}
+	 * @param attenuation {@link Vector3f}
+	 */
+	public Light(String name, Vector3f position, Vector3f color, Vector3f attenuation) {
 		super(name);
 		this.position = position;
-		this.color = colour;
+		this.color = color;
 		this.attenuation = attenuation;
 
 	}
-
-	// вернуть затухание
+	
+	@Override
 	public Vector3f getAttenuation() {
 		return attenuation;
 	}
-
+	
+	@Override
 	public Vector3f getPosition() {
 		return position;
 	}
-
+	
+	@Override
 	public void setPosition(Vector3f position) {
 		this.position = position;
 	}
 
+	@Override
 	public Vector3f getColor() {
 		return color;
 	}
 
+	@Override
 	public void setColor(Vector3f color) {
 		this.color = color;
 	}
